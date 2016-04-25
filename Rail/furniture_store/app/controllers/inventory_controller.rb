32558e07be3aad8f35b70bb9fc54505d04a63623
darktef <darktef@gmail.com>
2016-04-25 14:19:13 -0500

    update furniture store project

---
+++ Rail/furniture_store/app/controllers/inventory_controller.rb
@@ -0,0 +1,16 @@
+class InventoryController < ApplicationController
+  def all_products
+  	@products = Product.all
+  end
+
+  def one_product
+  	@product = Product.find(params[:id])
+  end
+
+  def by_category
+  	@product=Product.where("category=?",params[:category])	
+  end
+
+
+
+end
